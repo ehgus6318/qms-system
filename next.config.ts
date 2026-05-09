8a53b49bfaa17773f08dc9fc5d0694f5d6f70fdf
kim dohyun <ehgus6318@gmail.com>
2026-05-09 15:32:11 +0900

    work: save latest qms progress

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["59.12.17.38"],
 };
 
 export default nextConfig;
